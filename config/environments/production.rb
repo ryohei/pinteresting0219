0cad5be00417f02b1c7ba3ff16a34f57a2196371
Ryohei Fujita <ryohei@rento.jp>
2015-02-22 03:39:45 +0900

    change aws setting on production.rb for Tokyo region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
   #Paperclip with AWS
   config.paperclip_defaults = {
   :storage => :s3,
+  s3_host_name: s3-ap-northeast-1.amazonaws.com,
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
