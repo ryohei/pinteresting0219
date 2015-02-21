0984f1002a40687f2529fb2827e4b90f4389df8f
Ryohei Fujita <ryohei@rento.jp>
2015-02-22 04:22:44 +0900

    modified production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,13 +88,11 @@ Rails.application.configure do
   #Paperclip with AWS
   config.paperclip_defaults = {
   :storage => :s3,
-  :s3_credentials => "#{Rails.root}/config/s3.yml"
-
-  # {
-  #   :bucket => ENV['S3_BUCKET_NAME'],
-  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #   :s3_host_name: 's3-ap-northeast-1.amazonaws.com'
-  # }
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :s3_host_name: 's3-ap-northeast-1.amazonaws.com'
+   }
 }
 end
